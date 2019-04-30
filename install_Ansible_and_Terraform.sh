317e729ac69c94b635b8de2db145d2bb2c3cdc9a
haggai <haguyle@gmail.com>
2019-04-30 09:46:37 -0400

    update install script and created new files for Ansible and Teraform

--- install_Ansible_and_Terraform.sh
+++ install_Ansible_and_Terraform.sh
@@ -43,7 +43,7 @@ sudo apt-get install ansible -y
 ansible --version
 # Create keys to access our server
 ssh-keygen
-.ssh/kryptonite
+.ssh/yourkeynamehere # secret :0 >> ;)
 password shhhhhhhh # just kidding with you I wont tell you my key  password Dah:)
 
 #Access to ssh agent to allow the forwarding of the key
@@ -62,3 +62,27 @@ host_key_checking = False
 mkdir terransible
 # change into directory
 cd terransible
+
+# Setup AWS account
+#The following step are done MANUALLY! step-up by AWS console GUI
+#Step1: Navigate to IAM  > USERS
+#Step2: Click Add User >  give it a name > Under Access type select Programmatic access > click Next
+#step3: Set Permission for the user > click Attach existing policies directly
+#step4: Select AdministratorAccess > Next > review > create user
+#step5: DOWNLOAD credentials  !!
+
+
+# Configuring a new profile for AWS
+aws configure --profile superhero
+#enter your Access key ID
+#enter your Secret access key
+#set region name: us-east-1 (you can set it to you liking)
+# check if we connected
+aws ec2 describe-instances --profile superhero
+# setting  up AWS ROUTE53 (DNS) to set domains and keep the same name  to create hosted zone
+aws route53 create-reusable-delegation-set --caller-reference 1224 --profile superhero
+# copy your DNS setting for ease of USE
+
+# create these files in terransible
+touch main.tr terraform.tfvars variables.tf
+touch userdata aws_hosts wordpress.yml s3update.yml
