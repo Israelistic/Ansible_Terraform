60dfb4eadab18b86afa7a70cffb03efdf86d875b
haggai <haguyle@gmail.com>
2019-05-03 00:17:07 -0400

    added more logic to main.tf

--- terransible/main.tf
+++ terransible/main.tf
@@ -2,3 +2,16 @@ provider "aws" {
  region  = "${var.aws_region}"
  profile = "${var.aws_profile}"
 }
+
+#--------IAM-------
+
+#S3_access
+
+resource "aws_iam_instance_profile" "s3_access_profile {
+  name = "s3_access"
+  role = ${aws_iam_role.s3_access_role.name}
+}"
+
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+}
